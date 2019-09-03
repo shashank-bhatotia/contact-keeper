8c3fe1bf2e9b395a3833e8cf74c17a79c65112e8
shashank <shashankbhatotia@gmail.com>
2019-09-03 13:39:57 +0530

    Encrypt password

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 const User = require('../../../models/User');
 
@@ -18,20 +19,37 @@ router.post("/", [
   // password is 6 chars long
   check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
 
-], (req, res) => {
-
+], async (req, res) => {
   // find the validation errors in the request and wrap them in valid object
   const errors = validationResult(req);
-
   if(!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
 
-  User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  }).then(user => res.json(user));
+  const { name, email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    if (user) {
+      res.status(400).json({ msg: "User already exists!"});
+    }
+    user = new User({
+      name,
+      email,
+      password
+    })
+
+    // password encryption
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+    await user.save();
+
+    res.send('User created!');
+
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send('Serer Error!');
+  }
 })
 
 module.exports = router;
