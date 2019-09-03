b9ae5ea8dd6dac751c4d6d50e6a02c7c0ec1270f
shashank <shashankbhatotia@gmail.com>
2019-09-03 08:47:59 +0530

    Update server

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
 
+// connect to database
+connectDB();
+
+// set routes
 app.get('/', (req, res) => {
   res.json({
     message: "Welcome to the contact keeper API"
   });
 });
 
-// set routes
 app.use("/api/v1/users", require("./routes/api/v1/users"));
 app.use("/api/v1/auth", require("./routes/api/v1/auth"));
 app.use("/api/v1/contacts", require("./routes/api/v1/contacts"));
