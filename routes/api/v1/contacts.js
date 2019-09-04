7d68fef13d3ee45f4220c339b15b0cdf7a999e3d
shashank <shashankbhatotia@gmail.com>
2019-09-04 09:06:47 +0530

    Create Contact model and route

--- routes/api/v1/contacts.js
+++ routes/api/v1/contacts.js
@@ -1,12 +1,25 @@
 const express = require('express');
-
 const router = express.Router();
+
+const { check, validationResult } = require('express-validator');
+const User = require('../../../models/User');
+const Contact = require('../../../models/Contact');
+const auth = require('../../../middleware/auth');
 
 // @route   GET api/v1/contacts
 // @desc    Get all users contacts
 // @access  Private
-router.get("/", (req, res) => {
-  res.send('Get all contacts!');
+router.get("/", auth, async (req, res) => {
+  try {
+    // we have access to req.user.id from the auth middleware
+    // we want the contacts listed against a specific user id
+    // sort({ date: -1 }) will bring the most recent contact on the top
+    const contacts = await Contact.find({ user: req.user.id }).sort({ date: -1 });
+    res.json(contacts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error!');
+  }
 });
 
 // @route   POST api/v1/contacts/
